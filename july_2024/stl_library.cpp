3f7a2c2bd8d1eedb6c62ddf8cce1bcc177482607
MD HADIUZZAMAN ZAFRI <mdhadiuzzamanzafri@zafri.local>
2024-07-02 21:55:03 +0530

    added stl library

---
+++ july_2024/stl_library.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void library() {
+
+//----------------------------- Start of "vector" STL Library --------------------------------
+
+/*
+                vector
+    1. Vectors is a dynamic arrays change size when elements are added or deleted from them. 
+    2. Vector elements can be easily accessed and traversed using iterators. 
+    3. A vector stores elements in contiguous memory locations.
+
+    4. begin() - it returns an iterator pointing to the first element of the vector.
+    5. end() - it returns an iterator pointing to the element after the last element of the vector.
+    6. push_back() - it accepts a parameter and insert the element at the end
+    7. insert(position, element) - it is used to insert an element at a specified position.
+    8. erase() - it is used to delete a specific element
+    9. pop_back() - it deletes the last element and returns it to the calling function
+    10. front() - it returns a reference to the first element of the vector
+    11. back() - it returns a reference to the last element of the vector.
+    12. clear() - deletes all the elements from the vector.
+    13. empty() - to check if the vector is empty or not.
+    14. size() - returns the size of the vector
+
+*/
+
+vector<int> v1;
+vector<char> v2;
+vector<string> v3;
+
+//----------------------------- End of "vector" STL Library --------------------------------
+
+
+}
+
+
+
+
+int main() {
+    library();
+    return 0;
+}
